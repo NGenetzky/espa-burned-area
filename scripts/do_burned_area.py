caadb9c97ed0dd9047e881573edd3ebd422c9e58
NGenetzky <NGenetzky@usgs.gov>
2015-10-13 16:12:01 -0500

    Removed logIt function + minor fixes.

--- scripts/do_burned_area.py
+++ scripts/do_burned_area.py
@@ -21,23 +21,6 @@ from do_spectral_indices import SpectralIndices
 ERROR = 1
 SUCCESS = 0
 
-def logIt (msg, log_handler):
-    """Logs the user-specified message.
-    logIt logs the information to the logfile (if valid) or to stdout if the
-    logfile is None.
-
-    Args:
-      msg - message to be printed/logged
-      log_handler - log file handler; if None then print to stdout
-
-    Returns: nothing
-    """
-
-    if log_handler is None:
-        print msg
-    else:
-        log_handler.write (msg + '\n')
-
 
 #############################################################################
 # Created on April 10, 2014 by Gail Schmidt, USGS/EROS
